d2317ac614fdcc00a8704216fb754c51eba72b32
vuminhanhhust@gmail.com <anvuminh2004@gmail.com>
2025-10-01 10:51:32 +0700

    fix: fix SSR error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,30 @@
 'use client';
 import { useRoleState } from '@/store/useRoleState';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import Loading from './loading';
 
 export default function Page() {
   const role = useRoleState((state) => state.role);
+  const [mounted, setMounted] = useState(false);
+
   useEffect(() => {
+    setMounted(true);
     const accessToken = sessionStorage.getItem('accessToken');
-    if (accessToken !== null) {
+    if (accessToken) {
       if (role === 'ADMIN') {
-        window.location.href = 'dashboard/admin/location';
+        window.location.href = '/dashboard/admin/location';
       } else {
-        window.location.href = 'dashboard/user/booking';
+        window.location.href = '/dashboard/user/booking';
       }
     } else {
-      window.location.href = 'auth/sign-in';
+      window.location.href = '/auth/sign-in';
     }
   }, [role]);
 
-  return null;
+  if (!mounted) return null;
+  return (
+    <div className='flex h-screen items-center justify-center'>
+      <Loading />
+    </div>
+  );
 }
