6570a0a34fa0e8a5ea533f401ebfc7bd92c76ad7
vuminhanhhust@gmail.com <anvuminh2004@gmail.com>
2025-10-02 02:03:15 +0700

    fix: fix for title meeting when creating new meeting from calendar

--- src/components/ui/card-data.tsx
+++ src/components/ui/card-data.tsx
@@ -1,42 +1,77 @@
-'use client'
-import Loading from '@/app/loading'
-import { Card, CardAction, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
-import { timeToDate } from '@/features/profile/utils/convert-time-to-date'
-import { Time } from '@/features/profile/utils/format-time-range'
-import { useRouter } from 'next/navigation'
-import { useState } from 'react'
-import { toast } from 'sonner'
-import { Button } from './button'
+'use client';
+import Loading from '@/app/loading';
+import {
+  Card,
+  CardAction,
+  CardDescription,
+  CardHeader,
+  CardTitle
+} from '@/components/ui/card';
+import { timeToDate } from '@/features/profile/utils/convert-time-to-date';
+import { Time } from '@/features/profile/utils/format-time-range';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { toast } from 'sonner';
+import { Button } from './button';
 
-
-const CardData = ({data, startTime, endTime}:{data: any, startTime: Time | undefined, endTime: Time | undefined}) => {
-
-    const router = useRouter()
-    const start = timeToDate(startTime)
-    const end = timeToDate(endTime)
-    const [loading, setLoading] = useState<boolean>(false)
-    const handleBookClick = () =>{
-        if(start === undefined || end === undefined) {toast.warning('have something wrong')}
-        else{
-            setLoading(true)
-            router.push(`/dashboard/user/booking/new?meetingRoomId=${data.id}&startTime=${encodeURIComponent(start?.toISOString())}&endTime=${encodeURIComponent(end?.toISOString())}&title=${data.name}`)
-        }
+const CardData = ({
+  data,
+  startTime,
+  endTime,
+  titleBooking
+}: {
+  data: any;
+  startTime: Time | undefined;
+  endTime: Time | undefined;
+  titleBooking: string;
+}) => {
+  const router = useRouter();
+  const start = timeToDate(startTime);
+  const end = timeToDate(endTime);
+  const [loading, setLoading] = useState<boolean>(false);
+  const handleBookClick = () => {
+    if (start === undefined || end === undefined) {
+      toast.warning('have something wrong');
+    } else {
+      setLoading(true);
+      router.push(
+        `/dashboard/user/booking/new?meetingRoomId=${data.id}&startTime=${encodeURIComponent(start?.toISOString())}&endTime=${encodeURIComponent(end?.toISOString())}&title=${titleBooking}`
+      );
     }
+  };
   return (
     <Card className='mb-4 shadow-md'>
-        <CardHeader>
-            <CardTitle>{data.name}</CardTitle>
-            <CardDescription className='flex flex-col'>
-                <div><span className='font-bold'>Location:</span> {data.location.address}</div>
-                <div><span className='font-bold'>City: </span>{data.location.city}</div>
-                <div><span className='font-bold'>Status: </span>{data.active? 'Đang hoạt động':'Đã ngừng hoạt động'}</div>
-            </CardDescription>
-            <CardAction>
-                {loading ? <Loading />  : <Button size='sm' className='bg-blue-500 text-white hover:bg-blue-700 cursor-pointer' onClick={handleBookClick}>Book</Button>}
-            </CardAction>
-        </CardHeader>
+      <CardHeader>
+        <CardTitle>{data.name}</CardTitle>
+        <CardDescription className='flex flex-col'>
+          <div>
+            <span className='font-bold'>Location:</span> {data.location.address}
+          </div>
+          <div>
+            <span className='font-bold'>City: </span>
+            {data.location.city}
+          </div>
+          <div>
+            <span className='font-bold'>Status: </span>
+            {data.active ? 'Đang hoạt động' : 'Đã ngừng hoạt động'}
+          </div>
+        </CardDescription>
+        <CardAction>
+          {loading ? (
+            <Loading />
+          ) : (
+            <Button
+              size='sm'
+              className='cursor-pointer bg-blue-500 text-white hover:bg-blue-700'
+              onClick={handleBookClick}
+            >
+              Book
+            </Button>
+          )}
+        </CardAction>
+      </CardHeader>
     </Card>
-  )
-}
+  );
+};
 
-export default CardData
+export default CardData;
--- src/features/Calendar/components/add-new-event.tsx
+++ src/features/Calendar/components/add-new-event.tsx
@@ -1,79 +1,127 @@
-'use client'
-import Loading from '@/app/loading'
-import { Button } from '@/components/ui/button'
-import CardData from '@/components/ui/card-data'
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog'
-import { ScrollArea } from '@/components/ui/scroll-area'
-import { initData, Room } from '@/constants/apiRoom'
-import { formatTimeRange, Time } from '@/features/profile/utils/format-time-range'
-import React, { SetStateAction, useEffect, useState } from 'react'
-import { toast } from 'sonner'
+'use client';
+import Loading from '@/app/loading';
+import { Button } from '@/components/ui/button';
+import CardData from '@/components/ui/card-data';
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle
+} from '@/components/ui/dialog';
+import { ScrollArea } from '@/components/ui/scroll-area';
+import { initData, Room } from '@/constants/apiRoom';
+import {
+  formatTimeRange,
+  Time
+} from '@/features/profile/utils/format-time-range';
+import React, { SetStateAction, useEffect, useState } from 'react';
+import { toast } from 'sonner';
 
 type AddNewEventProp = {
-    startTime: Time| undefined,
-    endTime: Time | undefined,
-    dialogOpen: boolean,
-    setDialogOpen: React.Dispatch<SetStateAction<boolean>>,
-}
+  startTime: Time | undefined;
+  endTime: Time | undefined;
+  dialogOpen: boolean;
+  setDialogOpen: React.Dispatch<SetStateAction<boolean>>;
+};
 
-const AddnewEvent = ({startTime, endTime, dialogOpen, setDialogOpen}:AddNewEventProp) => {
-
-    
-    const [step, setStep] = useState<number>(1)
-    const [titleBooking, setTitleBooking] = useState<string>('')
-    const [rooms, setRooms] = useState<Room[] | undefined>()
-    const [loading, setLoading] = useState<boolean>(false)
-    const handleNextClick = () =>{
-        if(titleBooking === '') {
-            toast.warning("The meeting's name can't be empty")
-        }else{
-            setStep((prev) => prev + 1)
-        }
+const AddnewEvent = ({
+  startTime,
+  endTime,
+  dialogOpen,
+  setDialogOpen
+}: AddNewEventProp) => {
+  const [step, setStep] = useState<number>(1);
+  const [titleBooking, setTitleBooking] = useState<string>('');
+  const [rooms, setRooms] = useState<Room[] | undefined>();
+  const [loading, setLoading] = useState<boolean>(false);
+  const handleNextClick = () => {
+    if (titleBooking === '') {
+      toast.warning("The meeting's name can't be empty");
+    } else {
+      setStep((prev) => prev + 1);
     }
-    useEffect(() =>{
-    
-        async function fetchData() {
-          setLoading(true)
-          await initData.initialize()
-          const rooms = initData.getRooms({page: 1, limit: 9999})
-          setRooms(rooms.rooms)
-          setLoading(false)
-        }
-        fetchData()
-    },[])
+  };
+  useEffect(() => {
+    async function fetchData() {
+      setLoading(true);
+      await initData.initialize();
+      const rooms = initData.getRooms({ page: 1, limit: 9999 });
+      setRooms(rooms.rooms);
+      setLoading(false);
+    }
+    fetchData();
+  }, []);
   return (
     <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
-        <DialogContent className='p-10'>
-            <DialogHeader>
-                <DialogTitle className='text-red-500'>
-                    {step === 1 && "Thời gian tổ chức: "}
-                    {step === 2 && "Danh sách các phòng họp"}
-                    {step === 1 && <span className='text-black font-no text-xl/7'>{formatTimeRange(startTime, endTime)}</span>}
-                </DialogTitle>  
-            </DialogHeader>
-            {step === 1 &&(
-                <div className='flex flex-col gap-3'>
-                    <input type='text' className='h-10 px-1 text-xl ' autoFocus placeholder='Nhập tên cuộc họp' value={titleBooking} onChange={(e) => setTitleBooking(e.target.value)}/>
-                    <Button variant='destructive' className='bg-blue-500 hover:bg-blue-600 text-xl py-2' onClick={handleNextClick}>Tiếp</Button>
-                </div>
+      <DialogContent className='p-10'>
+        <DialogHeader>
+          <DialogTitle className='text-red-500'>
+            {step === 1 && 'Thời gian tổ chức: '}
+            {step === 2 && 'Danh sách các phòng họp'}
+            {step === 1 && (
+              <span className='font-no text-xl/7 text-black'>
+                {formatTimeRange(startTime, endTime)}
+              </span>
             )}
-            {step === 2 &&(
-                <div>
-                    {loading? <Loading /> 
-                    :<ScrollArea className='h-[40vh]'>
-                        {rooms?.map((item, index) => (
-                            <CardData data={item} key={index} startTime={startTime} endTime={endTime}/>
-                        ))}
-                    </ScrollArea>}
-                    <div className='flex flex-row gap-2 justify-center'>
-                        <Button variant='destructive' className='bg-blue-500 hover:bg-blue-600 text-xl py-2' onClick={() => setStep((prev) => prev - 1)}>Trước</Button>
-                        <Button variant='destructive' className='bg-blue-500 hover:bg-blue-600 text-xl py-2' onClick={() => setDialogOpen(false)}>Done</Button>
-                    </div>
-                </div>
+          </DialogTitle>
+        </DialogHeader>
+        {step === 1 && (
+          <div className='flex flex-col gap-3'>
+            <input
+              type='text'
+              className='h-10 px-1 text-xl'
+              autoFocus
+              placeholder='Nhập tên cuộc họp'
+              value={titleBooking}
+              onChange={(e) => setTitleBooking(e.target.value)}
+            />
+            <Button
+              variant='destructive'
+              className='bg-blue-500 py-2 text-xl hover:bg-blue-600'
+              onClick={handleNextClick}
+            >
+              Tiếp
+            </Button>
+          </div>
+        )}
+        {step === 2 && (
+          <div>
+            {loading && titleBooking !== undefined ? (
+              <Loading />
+            ) : (
+              <ScrollArea className='h-[40vh]'>
+                {rooms?.map((item, index) => (
+                  <CardData
+                    data={item}
+                    key={index}
+                    startTime={startTime}
+                    endTime={endTime}
+                    titleBooking={titleBooking}
+                  />
+                ))}
+              </ScrollArea>
             )}
-        </DialogContent>
+            <div className='flex flex-row justify-center gap-2'>
+              <Button
+                variant='destructive'
+                className='bg-blue-500 py-2 text-xl hover:bg-blue-600'
+                onClick={() => setStep((prev) => prev - 1)}
+              >
+                Trước
+              </Button>
+              <Button
+                variant='destructive'
+                className='bg-blue-500 py-2 text-xl hover:bg-blue-600'
+                onClick={() => setDialogOpen(false)}
+              >
+                Done
+              </Button>
+            </div>
+          </div>
+        )}
+      </DialogContent>
     </Dialog>
-  )
-}
+  );
+};
 
-export default AddnewEvent
+export default AddnewEvent;
